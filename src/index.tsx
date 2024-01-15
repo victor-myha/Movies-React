3edc1e3c62a191948a5f13e279d518f6cbdd0005
victor-myha-serec <viktor@seractech.co.uk>
2024-01-15 23:57:55 +0200

    [add]

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import { CssBaseline } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
